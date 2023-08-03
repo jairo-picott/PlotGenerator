d4d5248b1369dab5458b746341ed93b7254a33da
Jairo Basto Picott <107066101+jairo-picott@users.noreply.github.com>
2023-08-03 10:01:08 +0100

    Update proxy.js

--- netlify/functions/proxy/proxy.js
+++ netlify/functions/proxy/proxy.js
@@ -1,17 +1,28 @@
-// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
-const handler = async (event) => {
-  try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
-  }
-}
+const fetch = require('node-fetch');
 
-module.exports = { handler }
+exports.handler = async function(event, context) {
+    const { entity_id, start_time, end_time } = event.queryStringParameters;
+
+    try {
+        const response = await fetch(`http://192.168.0.100:8123/api/history/period/${start_time}?filter_entity_id=${entity_id}&end_time=${end_time}&minimal_response`, {
+            headers: {'Authorization': `Bearer ${process.env.HA_TOKEN}`}
+        });
+    
+        if (!response.ok) {
+            return {statusCode: response.status, body: response.statusText};
+        }
+    
+        const data = await response.json();
+    
+        return {
+            statusCode:200,
+            body: JSON.stringify(data),
+        };
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return {
+            statusCode: 500,
+            body: 'Error fetching data'
+        };
+    }  
+};
