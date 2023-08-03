4cdd2159dffc357126d0d5f18b776c2aa048fea5
Jairo Basto Picott <107066101+jairo-picott@users.noreply.github.com>
2023-08-03 11:22:44 +0100

    Update proxy.js

--- netlify/functions/proxy/proxy.js
+++ netlify/functions/proxy/proxy.js
@@ -1,9 +1,9 @@
-const fetch = require('node-fetch');
-
 exports.handler = async function(event, context) {
     const { entity_id, start_time, end_time } = event.queryStringParameters;
 
     try {
+        const { default: fetch } = await import('node-fetch');
+
         const response = await fetch(`http://192.168.0.100:8123/api/history/period/${start_time}?filter_entity_id=${entity_id}&end_time=${end_time}&minimal_response`, {
             headers: {'Authorization': `Bearer ${process.env.HA_TOKEN}`}
         });
