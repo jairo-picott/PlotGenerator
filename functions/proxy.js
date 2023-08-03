07508c82b4e9e38bcd8c34be896dbc409e4a722c
Jairo Basto Picott <107066101+jairo-picott@users.noreply.github.com>
2023-08-03 09:42:33 +0100

    Update proxy.js

--- functions/proxy.js
+++ functions/proxy.js
@@ -1,20 +1,28 @@
 const fetch = require('node-fetch');
 
 exports.handler = async function(event, context) {
-    const { entity_id } = event.queryStringParameters;
+    const { entity_id, start_time, end_time } = event.queryStringParameters;
 
-    const response = await fetch(`http://192.168.0.100:8123/api/history/period/2023-07-30?filter_entity_id=${entity_id}&end_time=23-07-30&minimal_response`, {
-        header: {'Authorization': `Bearer ${process.env.HA_TOKEN}`}
-    });
-
-    if (!response.ok) {
-        return {statusCode: response.status, body: response.statusText};
-    }
-
-    const data = await response.json();
-
-    return {
-        statusCode:200,
-        body: JSON.stringify(data),
-    };
+    try {
+        const response = await fetch(`http://192.168.0.100:8123/api/history/period/${start_time}?filter_entity_id=${entity_id}&end_time=${end_time}&minimal_response`, {
+            headers: {'Authorization': `Bearer ${process.env.HA_TOKEN}`}
+        });
+    
+        if (!response.ok) {
+            return {statusCode: response.status, body: response.statusText};
+        }
+    
+        const data = await response.json();
+    
+        return {
+            statusCode:200,
+            body: JSON.stringify(data),
+        };
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return {
+            statusCode: 500,
+            body: 'Error fetching data'
+        };
+    }  
 };
