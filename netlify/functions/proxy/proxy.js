84db8c6d96f5f26e04f648800dc0e67a2b929562
Jairo Picott <jairobpicott@gmail.com>
2023-08-07 09:16:33 +0100

    Update proxy.js

--- netlify/functions/proxy/proxy.js
+++ netlify/functions/proxy/proxy.js
@@ -1,66 +1,42 @@
-const Plotly = require('plotly.js');
-
-exports.handler = async function(event, context) {
-    //Ensure this function only runs for POST requests
-    if(event.httpMethod !== "POST") {
-        return {statusCode: 405, body: "Mthod Not Allowed"};
-    }
-
-    //Parse the HSON data sent in the request
-    let data;
-    try {
-        data = JSON.parse(event.body);
-    } catch (error) {
-        console.log("Error parsing JSON: ", error);
-        return {
-            statusCode: 400,
-            body: JSON.stringify({error: "Bad request body" }),
-        };
-    }
-
-    //Loop through each array in the data to create a separate series for each Entity ID passed
-    let traces = data.map(sensorData => {
-        // Extract valued from sensorData
-        const entityID = sensorData[0]["entity_id"];
-        const friendlyName = sensorData[0]["attributes"]["friendly_name"];
-        const xValues = sensorData.slice(1).map(item => new Date(item["last_changed"]));
-        const yValues = sensorData.slice(1).map(item => parseFloat(item["state"]));
-
-        return {
-            x: xValues,
-            y: yValues,
-            mode: 'lines',
-            name: friendlyName
-        };
-    });
-
-    var layout = {
-        title: 'Twin data viewer',
-        xaxis: {
-            title: 'Date',
-            showgrid: false,
-            zeroline: false,
-        },
-        yaxis: {
-            title: 'Measurement',
-            showline: false
-        },
-        showlegend: true
-    };
-
-    //Generate the plot
-
-    const figure = {'data': traces, layout: layout};
-    const imgOpts = {format: 'png', width: 1000, height: 500};
-
-    //Convert plot to image
-    var img = await Plotly.toImage(figure, imgOpts);
-
-    //Return the image in the response
+exports.handler = async (event, context) => {
+    // Parse incoming JSON data from request body
+    const data = JSON.parse(event.body);
+  
+    // Generate HTML with Plotly script
+    const html = `
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+        </head>
+        <body>
+          <div id="myDiv"></div>
+          <script>
+            const data = ${JSON.stringify(data)};
+  
+            const traces = data.map(item => ({
+              x: item.map(point => new Date(point.last_changed)),
+              y: item.map(point => point.state),
+              mode: 'lines',
+              name: item[0].entity_id,
+            }));
+  
+            const layout = {
+              title: 'Sensor Data',
+            };
+  
+            Plotly.newPlot('myDiv', traces, layout);
+          </script>
+        </body>
+      </html>
+    `;
+  
     return {
-        statusCode: 200,
-        headers: {'Content-Type': 'image/png'},
-        body: img.toString('base64'),
-        isBase64Encoded: true
+      statusCode: 200,
+      headers: {
+        "Content-Type": "text/html",
+      },
+      body: html,
     };
-};
+  };
+  
